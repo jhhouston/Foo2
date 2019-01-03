01e25a4890d97c80ea9da9e01d21c3e0f74a66bf
James Houston II <jhhoustonii@gmail.com>
2019-01-03 15:52:26 -0600

    BookImageUpload

---
+++ BookImageUpload/BookImageUpload/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace BookImageUpload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bookInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.booksDataSet = new BookImageUpload.booksDataSet();
+            this.bookInfoTableAdapter = new BookImageUpload.booksDataSetTableAdapters.bookInfoTableAdapter();
+            this.tableAdapterManager = new BookImageUpload.booksDataSetTableAdapters.TableAdapterManager();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.bookInfoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bookInfoBindingSource
+            // 
+            this.bookInfoBindingSource.DataMember = "bookInfo";
+            this.bookInfoBindingSource.DataSource = this.booksDataSet;
+            // 
+            // booksDataSet
+            // 
+            this.booksDataSet.DataSetName = "booksDataSet";
+            this.booksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookInfoTableAdapter
+            // 
+            this.bookInfoTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.bookInfoTableAdapter = this.bookInfoTableAdapter;
+            this.tableAdapterManager.UpdateOrder = BookImageUpload.booksDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(550, 142);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(15, 187);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(813, 251);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.ListView_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Author name";
+            this.columnHeader1.Width = 200;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Author and Title :";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(239, 153);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(550, 98);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(550, 57);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 6;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(330, 59);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(138, 20);
+            this.txtTitle.TabIndex = 7;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(330, 98);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(138, 20);
+            this.txtAuthor.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Title:";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Book Title";
+            this.columnHeader2.Width = 149;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bookInfoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private booksDataSet booksDataSet;
+        private System.Windows.Forms.BindingSource bookInfoBindingSource;
+        private booksDataSetTableAdapters.bookInfoTableAdapter bookInfoTableAdapter;
+        private booksDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ BookImageUpload/BookImageUpload/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookImageUpload
+{
+    public partial class Form1 : Form
+    {
+        //Class variables
+        string fileName;
+        //string title;
+        //string author;
+        List<bookInfo>list; //Database Name <bookInfo> made form Entity Framework
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BookInfoBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.bookInfoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.booksDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'booksDataSet.bookInfo' table. You can move, or remove it, as needed.
+            this.bookInfoTableAdapter.Fill(this.booksDataSet.bookInfo);
+
+        }
+
+        private void FillByToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.bookInfoTableAdapter.FillBy(this.booksDataSet.bookInfo);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        //Binary converter
+        Image ConvertBinaryToImage(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+
+        private void ListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView.FocusedItem !=null)
+            {
+                pictureBox.Image = ConvertBinaryToImage(list[listView.FocusedItem.Index].CoverImage);
+                
+            }
+        }
+
+
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            //Opens windows file folder & Filter
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PNG|*.png", ValidateNames = true, Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //if it opens 1) Take file name, 2) change label text on form 3) load the image to view
+                    fileName = ofd.FileName;                    
+                    pictureBox.Image = Image.FromFile(fileName);
+
+                }
+            }
+        }
+
+        //Convert image to binary
+        byte[] ConvertImageToBinary(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+
+        private async void BtnSave_Click(object sender, EventArgs e)
+        {
+            //booksEntities name created in Entity Framework wizard
+            using (booksEntities db = new booksEntities())
+            {
+                                                  //Title     //Author                 //CoverArt
+                bookInfo info = new bookInfo() {Title = txtTitle.Text, Author = txtAuthor.Text, CoverImage = ConvertImageToBinary(pictureBox.Image)};
+                db.bookInfoes.Add(info);
+                await db.SaveChangesAsync();
+
+               
+
+
+                MessageBox.Show("You have saved the info", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            listView.Items.Clear();
+            using (booksEntities db = new booksEntities())
+            {
+                list = db.bookInfoes.ToList();
+                foreach (bookInfo info in list)
+                {
+                    ListViewItem item = new ListViewItem(info.Author);
+                   
+                    //Populating Columns
+                    listView.Items.Add(item);                    
+                    item.SubItems.Add(info.Title);
+                }
+
+
+            }
+        }
+    }
+}
